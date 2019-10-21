ff69f966bb8b60768a0cb67fb1bed48a55b32d66
Romario Bennett <romario_bennett@outlook.com>
2019-10-21 01:49:04 -0500

    edits2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,34 +9,66 @@ window.onload = function() {
     board1.forEach(i=>{i.className="square";});
     
     
+//var i;
+//var board2 = document.getElementsByClassName("square");
 
-   //document.getElementById("board").addEventListener('click',function ()
-    //{
-   // console.log("a click");
-    //var i;
-    //var board2 = document.getElementById("board").querySelectorAll("div");
 
-    //for (i = 0;i<board2.length;i++){
+//for (i = 0;i<board2.length;i++){
+
+  //board2[i].addEventListener("click",myFunction);
+
+//}
+
+//function myFunction(){
+
+  
+  //console.log("a click");
+  //board2[0].innerHTML = "X";
+//}
+   
+var board2 = document.getElementById("board").querySelectorAll("div");
+var i;
+
+
+
+
+let letterChange = false;
+for (i = 0;i<board2.length;i++){
+  var board3 = board2[i];
+  
+board3.onclick = function() {
+  
+  if (!letterChange && board3){
+  var i;
+  this.classList.add("X");
+  this.innerHTML = "X";
+  console.log("a click");
+
+  letterChange = true;
+  } 
+  else
+  {
+  var i;
+  this.classList.add("O");
+  this.innerHTML = "O";
+  console.log("a click");
     
-    //board2[i].innerHTML="X";
-    //}
-    
-     //validation code to see State field is mandatory.  
-   // });
-   var i;
-   var board2 = document.getElementsByClassName("square");
-
-   for (i = 0;i<board2.length;i++){
-   board2[i].addEventListener("click", myFunction);
-   //board2[1].addEventListener("click", myFunction);
-   } 
-   function myFunction() {
-    console.log("a click");
-    board2[0].innerHTML = "X";
-    //var board2 = document.getElementsByClassName("square");
-    //document.getElementbyId("div");
-    //board2[i].innerHTML = "X";
+  letterChange = false;
   }
+  
+}
+
+
+
+
+}
+
+
+
+
+
+
+
       
 }
 //}
